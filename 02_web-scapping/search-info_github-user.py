1f4fbb75cec6a4e02577bce743b26662ebd367de
Rashindoun <266145107+Rashindoun@users.noreply.github.com>
2026-03-08 16:36:07 +0100

    update github-info-user

--- 02_web-scapping/search-info_github-user.py
+++ 02_web-scapping/search-info_github-user.py
@@ -1,6 +1,8 @@
 import requests 
 
+#fonction principal du projet github-user-info
 def user_information():
+    #gestion du nombre de requète en fonction de la variable n_requete (penser ajouter aussi un système de timer)
     n_requetes = 0
     while True:
         if n_requetes < 30:
@@ -12,18 +14,30 @@ def user_information():
             print("vous les vous bien attendre 1H")
             break
         user=input("quel est le nom de l'utilisateur que vous chercher : ")
-        n_requetes +=1
-        r = requests.get("https://api.github.com/users/"+user)
-        if r.status_code == 200:
-            data = r.json()
-            print("voicit le nom de l'utilisateur : " ,data.get('login',""))
-            print("voicit l'ID de l'utilisitateur : " ,data.get('id',))
-            print("voicit la bio de l'utilisateur : " ,data.get('bio',))
-            print("voicit le nombre de repo qu'a l'utilisateur : " ,data.get('public_repos',))
-            print("voicit le nombre de followers de l'utilisateur : " ,data.get('followers',))
+        n_requetes +=2
+        response = requests.get(f"https://api.github.com/users/{user}")
+        response_repo = requests.get(f"https://api.github.com/users/{user}/repos")
+        if response.status_code == 200:
+            data = response.json()
+            donnés = response_repo.json()
+            print(f"L'utilisateur: {data.get('login')}")
+            print("📊 Infos :")
+            print(f"ID: {data.get('id')}")
+            print(f"Bio: {data.get('bio')}")
+            print(f"followers: {data.get('followers')}")
+            print("📚 Ses repos :")
+            print(f"Nombre de repo: {data.get('public_repos')}")
+            n = 0
+            for donné in donnés:
+                print(f"{n}. {donné['name']} (⭐ {donné['stargazers_count']})")
+                n += 1
         else:
-            print(f"attention , nous avons rencontrer une erreur lors du processus : {r.status_code}")
-            break
+            if response.status_code != 200:
+                print(f"attention , nous avons rencontrer une erreur lors du processus : {response.status_code}")
+                break
+            else:
+                print(f"attention , nous avons rencontrer une erreur lors du processus : {response_repo.status_code}")
+        # gestion de la boucle
         choix = input("voulez-vous utiliser encore le programme (O/n): ")
         if choix == "O":
             continue
