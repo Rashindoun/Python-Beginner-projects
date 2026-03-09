5b11c0cf5237fefb5a7924ffb72ef10b8966108f
Rashindoun <266145107+Rashindoun@users.noreply.github.com>
2026-03-09 21:03:47 +0100

    fix some issue of contacts manager

--- 01_Base/contact_manager.py
+++ 01_Base/contact_manager.py
@@ -20,6 +20,7 @@ contacts= {
 }
 
 def add_contacts():
+    global contacts
     while True:
         n = len(contacts)+1
         new_contacts_name = input("quel est le nom de se contacts: ")
@@ -30,7 +31,7 @@ def add_contacts():
             print("numéro de téléphone non pris en charge (nb de caractère)") 
             continue
         choice=input(f"es ce que vous voulez ajouter: {new_contacts_name} au numéro {new_contacts_phone_num} (O/n): ")
-        if choice == "O" or choix == "o":
+        if choice == "O" or choice == "o":
             dict_temp = {
                 f"contact{n}":{
                     "name":new_contacts_name,
@@ -39,10 +40,8 @@ def add_contacts():
             }
             new_dict = dict_temp | contacts
             contacts = new_dict | contacts
-            #contacts[f"contact{n}"]["name"]=new_contacts_name
-            #contacts[f"contact{n}"]["numéro de téléphone"]=new_contacts_phone_num
             print(f"{new_contacts_name} a bien été ajouté")
-        elif choice == "n" or choix == "N":
+        elif choice == "n" or choice == "N":
             print(f"{new_contacts_name} n'a pas été ajouté")
         print("*"*10)
         more_information_choice = input(f"vous les vous ajouter plus d'information vis-à-vis de {new_contacts_name} ? (O/n): ")
@@ -50,28 +49,23 @@ def add_contacts():
             more_information_choice_email = input("voulez vous ajoutez un email ? (O/n) : ")
             if more_information_choice_email == "O" or more_information_choice_email == "o": 
                 new_contacts_email = input(f"quel est l'email de {new_contacts_name} : ")
-                contacts[f"contacts{n}"]["email"] = new_contacts_email
+                contacts[f"contact{n}"]["email"] = new_contacts_email
             else:
                 pass
             more_information_choice_contry = input("voulez vous ajoutez un pays ? (O/n) : ")
             if more_information_choice_contry == "O" or more_information_choice_contry == "o":
                 new_contacts_contry = input (f"quel est le pays de résidence de {new_contacts_name} : ")
-                contacts[f"contacts{n}"]["pays"] = new_contacts_contry
+                contacts[f"contact{n}"]["pays"] = new_contacts_contry
             else:
                 pass
-            print(f"{new_contacts_name} a pour email : {new_contacts_email} et pour pays de résidence : {new_contacts_pays}")
-        choix = input("voulez vous ajoutez d'autre personne ? (O/n): ")
-        if choix == "O" or choix == "o":
-            continue
-        elif choix == "n" or choix == "N":
-            menu()
+        verification()
 
 
 def del_contacts():
     while True:
         del_name = input("tapez le nom de celui que vous voulez supprimer : ")
-        choix=input("es ce que vous voulez supprimer :"+del_name+" O/n")                         
-        max = len(contacts_base)                  
+        choix=input(f"es ce que vous voulez supprimer :{del_name} O/n")                         
+        max = len(contacts)                  
         for i in range(1, max + 1):               
             contact = contacts.get(f"contact{i}")  
             if contact != None and contact.get("name") == del_name:     
@@ -80,44 +74,64 @@ def del_contacts():
                     print(del_name,"a bien été supprimer")
                 elif choix == "n" or choix == "N":
                     print(del_name,"a pas été supprimer")
-        choix_continuer = input("voulez vous supprimer d'autre personne ? (O/n): ")
-        if choix_continuer == "O" or choix_continuer == "o":
-            continue
-        elif choix_continuer == "n" or choix_continuer == "N":
-            menu()
+        verification()
 
 def show_contacts():
-    if len(contacts) > 0:
-        print("voicit le nom de vos contacts :",contacts.keys())
-        print("voicit le numéro de téléphone de vos contacts",contacts.values())
-    else:
-        print("vous avez pas de contacts a l'heure actuel")
-        choix = input("Comme vous avez pas de contacts . Es ce que il serait pas donc le temps dans ajouter quelque un : O/n")
-        if choix == "O" or choix == "o":
-            print("commençons donc maintenant :")
-            add_contacts()
-            print(del_name,"a bien été supprimer")
-        elif choix == "n" or choix == "N":
-            print("domages peut etre une prochaine fois")
-            menu()
+    while True:
+        if len(contacts) > 0:
+            contacts_name = {}
+            for  i in range(1,len(contacts)+1):
+                contacts_name[i] = contacts[f"contact{i}"]["name"]
+            print(f"voicit le nom de vos contacts :{contacts_name.values()}")
+        else:
+            print("vous avez pas de contacts a l'heure actuel")
+            choix = input("Comme vous avez pas de contacts . Es ce que il serait pas donc le temps dans ajouter quelque un : O/n")
+            if choix == "O" or choix == "o":
+                print("commençons donc maintenant :")
+                add_contacts()
+            elif choix == "n" or choix == "N":
+                print("domages peut etre une prochaine fois")
+                menu()
+        verification()
 
 def search_contacts():
-    if len(contacts) > 0:
-        search =input("quel est le nom du contact que vous voulez cherchez : ")
-        name = search in contacts
-        if name == True:
-            print("ce contact existe belle et bien")
+    while True:
+        if len(contacts) > 0:
+            search = input("quel est le nom du contact que vous voulez cherchez : ")
+            bouche_troue = False
+            contacts_name = {}
+            for i in range(1,len(contacts)+1):
+                contacts_name[i] = contacts[f"contact{i}"]["name"]
+                match = contacts_name[i]
+                if  match == search:
+                    print(f"{search} existe belle est bien")
+                    print(f"les information suivante de {search} sont : {contacts[f"contact{i}"]}")
+                    bouche_troue = True # trouver une meilleur solution
+                    break
+            if bouche_troue != True:
+                print(f"{search} est introuvable")
+
         else:
-            print("ce contact n'existe pas")
-    else:
-        print("vous avez pas de contacts a l'heure actuel")
-        choix = input("Comme vous avez pas de contacts . Es ce que il serait pas donc le temps dans ajouter quelque un : O/n")
-        if choix == "O" or choix == "o":
-            print("commençons donc maintenant :")
-            add_contacts()
-            print(del_name,"a bien été supprimer")
-        elif choix == "n" or choix == "N":
-            print("domages peut etre une prochaine fois")
+            print("vous avez pas de contacts a l'heure actuel")
+            choix = input("Comme vous avez pas de contacts . Es ce que il serait pas donc le temps dans ajouter quelque un : O/n")
+            if choix == "O" or choix == "o":
+                print("commençons donc maintenant :")
+                add_contacts()
+            elif choix == "n" or choix == "N":
+                print("domages peut etre une prochaine fois")
+                menu()
+        verification()
+
+def verification():
+    while True:
+        choix_continuer = input("vous les vous continuer vos action (O/n) : ")
+        if choix_continuer == "O" or choix_continuer == "o":
+            return
+        elif choix_continuer == "n" or choix_continuer == "N":
+            menu()
+        else:
+            print(f"choix non pris en charge {choix_continuer}")
+            continue
 
 def menu():
     print("*"*38)
@@ -141,5 +155,7 @@ def menu():
         search_contacts()
     elif choix == 5:
         print("A une prochaine fois!")
+        return
+
 
 menu()
