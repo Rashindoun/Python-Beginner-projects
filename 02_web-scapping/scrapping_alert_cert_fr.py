049225fa414ae28f509c49813e81bdd853061895
Rashindoun <266145107+Rashindoun@users.noreply.github.com>
2026-03-11 20:17:21 +0100

    add scrapping_alerts_cert_fr

---
+++ 02_web-scapping/scrapping_alert_cert_fr.py
@@ -0,0 +1,21 @@
+import requests
+from bs4 import BeautifulSoup
+
+def scraping_alert_cert_fr():
+    responce = requests.get('https://www.cert.ssi.gouv.fr/')
+    main_page = responce.content
+    if responce.status_code == 200:
+        soup = BeautifulSoup(main_page,'html.parser')
+        alerts_cert_fr = soup.find_all("div" , class_ = "item cert-alert open" , limit = 100)
+        for i in range(0,len(alerts_cert_fr)):
+            alerts_cert = alerts_cert_fr[i]
+            alerts_cert_title = alerts_cert.find_all("a")
+            alerts_date = alerts_cert.find("span",class_ = "item-date")
+            alerts_title = alerts_cert_title[1]
+            alerts_title = alerts_title.get_text()
+            alerts_date = alerts_date.get_text()
+            print(f"voicit les alerte en court : {alerts_title} depuis {alerts_date}")
+    elif responce.status_code != 200:
+        print(f"nous avons rencontrer une erreur ({responce.status_code})")
+
+scraping_alert_cert_fr()
