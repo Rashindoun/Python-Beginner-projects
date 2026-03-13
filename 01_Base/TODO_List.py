81c0afaf30862b0e06e4b3c235d58e7c4773ef64
Rashindoun <266145107+Rashindoun@users.noreply.github.com>
2026-03-13 20:49:15 +0100

    fix TODO list

--- 01_Base/TODO_List.py
+++ 01_Base/TODO_List.py
@@ -1,7 +1,4 @@
-import time 
 import json
-
-tasks = ["Dev TODO list en python","faire le repassage","faire a manger","abandonner le projet git en python"]
 
 def show_task():
     print("-"*3 ," Show task " , "-"*3)
@@ -17,7 +14,7 @@ def show_task():
         add_task()
         
 def add_task():
-    print("-"*3 ," remove task " , "-"*3)
+    print("-"*3 ," add task " , "-"*3)
     while True:
         choice = input("vous les vous ajouter des tache a votre TODO(O/n): ")
         if choice == "O":
@@ -36,41 +33,61 @@ def add_task():
         break
 
 def del_task():
-    print("-"*3 ," add task " , "-"*3)
-    del_index_number = int(input("numéro de la tache a supprimer : "))
-    tasks.pop(del_index_number-1)
-    print("la tache a bien été supprimé")
+    while True:
+        print("-"*3 ," remove task " , "-"*3)
+        del_index_number = int(input("numéro de la tache a supprimer : "))
+        if del_index_number < len(tasks):
+            tasks.pop(del_index_number-1)
+            print("la tache a bien été supprimé")
+        else:
+            print("nous avons rencontrer un problème soit tentative de supprimer une tache inexistante")
+            continue
+        break
     
 def modify_task():
     while True:
+        print("-"*3 ," modify task " , "-"*3)
         choice = input("vous les vous changer (contenu/ordre): ")
         if choice == "ordre":
             temp_task = []
             n = int(input("numéro de la tache a rétrograder"))-1
             y = int(input("numéro de la tache a élever"))-1
-            temp_task.extend((tasks[n],tasks[y]))
-            tasks[n]=temp_task[1]
-            tasks[y]=temp_task[0]
+            if n < len(tasks) and y < len(tasks):
+                temp_task.extend((tasks[n],tasks[y]))
+                tasks[n]=temp_task[1]
+                tasks[y]=temp_task[0]
+                break
+            else:
+                print("nous avons rencontrer un problème soit tentative de modifier une tache inexistante")
+                continue
         elif choice == "contenu":
             n = int(input("numéro de la tache a changer"))-1
-            new_content = intput("quel est le nouveaux contenue de la tache: ")
-            tasks[n] = new_content
+            if n < len(tasks):
+                new_content = input("quel est le nouveaux contenue de la tache: ")
+                tasks[n] = new_content
+                break
+            else:
+                print("nous avons rencontrer un problème soit tentative de modifier une tache inexistante")
+                continue
         else:
             print(f"le format de réponse réponse n'est pas bon: {choice}")
             continue
         break
 
 def export():
-    save = task
+    save = json.dumps(tasks)
     with open("save_task.json" , "w+") as file:   
         file.write(save)
+
 def import_task():
-    with open("save_task.json" , "r+") as file:
-        
+    with open("save_task.json" , "r") as file:
+        tasks = json.load(file)
+    return tasks
 
 def menu():
     while True:
-        time.sleep(3)
+        global tasks
+        tasks = import_task()
         print("-"*3 ," Menu " , "-"*3)
         print("1. vous les vous voire vos tache en cour")
         print("2. vous les vous ajouter des tache a faire")
@@ -89,5 +106,6 @@ def menu():
             modify_task()
         elif choice == 5:
             break
+        export()
 
 menu()
